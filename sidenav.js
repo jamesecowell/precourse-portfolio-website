484ad0b6c978c42dfb9cd8723eb9a292ac9b5176
James Cowell <jamescowell@Jamess-MacBook-Pro.local>
2020-01-26 21:41:47 +0000

    changed to sidenav (not currently working)

---
+++ sidenav.js
@@ -0,0 +1,9 @@
+// Set width of sidenav to 250px
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+
+// Set width of sidenav to 0
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
